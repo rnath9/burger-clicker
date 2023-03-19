16fe74e25849efae7b4a2417715a38c0bdb8bf5f
jdr322@cornell.edu <jdr322@Joaquins-Laptop>
2023-03-19 15:47:57 -0400

    Implemented Keeping Track of Score
    - Score increments when the burger is clicked
    - Font, size, and location up to change

--- bin/main.ml
+++ bin/main.ml
@@ -1,32 +1,41 @@
 module G = Graphics
 
-let main () =
-  let a = G.open_graph " 1200X800" in
-  a;
+type information = { mutable burgers : int }
+
+let stats = { burgers = 0 }
+
+let background a =
   G.set_window_title "Burger Clicker";
-  G.set_color G.black;
-  G.fill_rect 0 0 (G.size_x a) (G.size_y a);
   let background = Png.load_as_rgb24 "BurgerBackground.png" [] in
   let display_background = Graphic_image.of_image background in
   let img = Png.load_as_rgb24 "ColoredWhopperFixed.png" [] in
   let display_image = Graphic_image.of_image img in
   G.draw_image display_background 0 0;
-  G.draw_image display_image ((G.size_x a / 2) - 182) ((G.size_y a / 2) - 48)
+  G.draw_image display_image ((G.size_x a / 2) - 182) ((G.size_y a / 2) - 48);
+  G.set_color (G.rgb 51 44 80)
 
-let rec interactive () =
+let main () =
+  let a = G.open_graph " 1200X800" in
+  background a
+
+let rec interactive a () =
   let event = G.wait_next_event [ G.Key_pressed; G.Button_down ] in
   if event.button then (
     if
       event.mouse_x > 400 && event.mouse_x < 540 && event.mouse_y > 350
       && event.mouse_y < 450
-    then print_endline "hit";
+    then stats.burgers <- stats.burgers + 1;
+    background a;
+    G.moveto 420 685;
+    Graphics.set_font "-*-fixed-medium-r-semicondensed--75-*-*-*-*-*-iso8859-1";
+    G.draw_string (string_of_int stats.burgers);
+
     if event.key == 'q' then (
       print_string "Thanks For Playing Burger Clicker!";
       exit 0)
-    else print_char event.key;
-    interactive ())
+    else interactive a ())
 
 let () =
   main ();
   print_newline ();
-  interactive ()
+  interactive (main ()) ()
