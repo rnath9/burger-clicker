92023bb91329d11591966fd5fa1fcf1ef62aae7d
Rahul <rrn9@cornell.edu>
2023-04-03 23:16:59 -0400

    Changed library from Graphics to Raylib
    - Updated all of main.ml to suit the Raylib library's functions
    - Edited dune dependencies

--- bin/main.ml
+++ bin/main.ml
@@ -1,40 +1,47 @@
-module G = Graphics
+module R = Raylib
 module H = Help
 
 let stats = H.init
 
-let background a =
-  G.set_window_title "Burger Clicker";
-  let background = Png.load_as_rgb24 "BurgerBackground.png" [] in
-  let display_background = Graphic_image.of_image background in
-  let img = Png.load_as_rgb24 "ColoredWhopperFixed.png" [] in
-  let display_image = Graphic_image.of_image img in
-  G.draw_image display_background 0 0;
-  G.draw_image display_image ((G.size_x a / 2) - 182) 352;
-  G.set_color (G.rgb 51 44 80)
+(*move state to stats later*)
+let state = ref 0
 
-let main () =
-  let a = G.open_graph " 1200X800" in
-  background a
+let setup () =
+  R.init_window 1200 800 "Burger Clicker";
+  R.set_target_fps 60;
+  let bg_image = R.load_image "BurgerBackground.png" in
+  let bg_texture = R.load_texture_from_image bg_image in
+  R.unload_image bg_image;
+  let burger_image = R.load_image "Transparent_Burger.png" in
+  let burger_texture = R.load_texture_from_image burger_image in
+  R.unload_image burger_image;
+  (bg_texture, burger_texture)
 
-let rec interactive a () =
-  let event = G.wait_next_event [ G.Key_pressed; G.Button_down ] in
-  if event.button then (
-    if
-      event.mouse_x > 400 && event.mouse_x < 540 && event.mouse_y > 350
-      && event.mouse_y < 450
-    then H.increment_burger stats;
-    background a;
-    G.moveto 420 685;
-    Graphics.set_font "-*-fixed-medium-r-semicondensed--75-*-*-*-*-*-iso8859-1";
-    G.draw_string (string_of_int stats.burgers);
+let burger_hitbox = R.Rectangle.create 425. 350. 100. 100.
 
-    if event.key == 'q' then (
-      print_string "Thanks For Playing Burger Clicker!";
-      exit 0)
-    else interactive a ())
+let rec loop texture =
+  let bg = fst texture in
+  let burger = snd texture in
+  match R.window_should_close () with
+  | true -> R.close_window ()
+  | _ ->
+      R.begin_drawing ();
+      R.clear_background R.Color.raywhite;
+      R.draw_texture bg 0 0 R.Color.raywhite;
+      R.draw_texture burger 425 350 R.Color.raywhite;
+      if R.is_mouse_button_down R.MouseButton.Left = false then state := 0;
+      let mouse_point = R.get_mouse_position () in
+      if R.check_collision_point_rec mouse_point burger_hitbox then
+        (*we can even switch the burger to look like you've hovered over it!*)
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 then (
+            state := 1;
+            H.increment_burger stats);
+      R.draw_text
+        (string_of_int stats.burgers)
+        425 30 100
+        (R.Color.create 50 42 79 255);
+      R.end_drawing ();
+      loop texture
 
-let () =
-  main ();
-  print_newline ();
-  interactive (main ()) ()
+let () = setup () |> loop
