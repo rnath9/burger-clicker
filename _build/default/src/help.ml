ec00bb4766525673e8ede4d64a0fa047296d3576
sjr275 <sjr275@cornell.edu>
2023-04-06 16:27:44 -0400

    Added items to the shop
    - Created items in the shop and prices that correspond to them
    - Created new hitboxes for all the items
    - Created conditions for buying the item and spending the burgers

---
+++ _build/default/src/help.ml
@@ -0,0 +1,104 @@
+type information = {
+  mutable burgers : int;
+  mutable bps : int;
+  mutable click_power : int;
+}
+
+let burger_init = { burgers = 0; bps = 0; click_power = 1 }
+let increment_burger_click t = t.burgers <- t.burgers + t.click_power
+let decrease_burger_spend t price = t.burgers <- t.burgers - price
+
+let increment_bps t item =
+  t.bps <-
+    (t.bps
+    +
+    match item with
+    | "spatula" -> 1
+    | "grilling dad" -> 5
+    | "burger tree" -> 50
+    | "food truck" -> 250
+    | "burger lab" -> 1500
+    | "burger wormhole" -> 10000
+    | _ -> failwith "attempted to increase bps with invalid item")
+
+let increment_burger_bps t = t.burgers <- t.burgers + t.bps
+let increment_click_power t mult = t.click_power <- t.click_power * mult
+
+type powerups_list = {
+  mutable sauce : bool;
+  mutable secret_sauce : bool;
+  mutable spatula : int;
+  mutable grilling_dad : int;
+  mutable burger_tree : int;
+  mutable food_truck : int;
+  mutable burger_lab : int;
+  mutable burger_wormhole : int;
+}
+
+type price_list = {
+  mutable sauce_price : int;
+  mutable secret_sauce_price : int;
+  mutable spatula_price : int;
+  mutable grilling_dad_price : int;
+  mutable burger_tree_price : int;
+  mutable food_truck_price : int;
+  mutable burger_lab_price : int;
+  mutable burger_wormhole_price : int;
+}
+
+let item_init =
+  {
+    sauce = true;
+    secret_sauce = true;
+    spatula = 0;
+    grilling_dad = 0;
+    burger_tree = 0;
+    food_truck = 0;
+    burger_lab = 0;
+    burger_wormhole = 0;
+  }
+
+let item_price_init =
+  {
+    sauce_price = 1000;
+    secret_sauce_price = 50000;
+    spatula_price = 15;
+    grilling_dad_price = 100;
+    burger_tree_price = 1500;
+    food_truck_price = 10000;
+    burger_lab_price = 100000;
+    burger_wormhole_price = 1000000;
+  }
+
+let increase_price (price : int) =
+  (price |> float_of_int) *. 1.33 |> int_of_float
+
+let increment_item p item (pr : price_list) =
+  match item with
+  | "sauce" -> p.sauce <- false
+  | "secret sauce" -> p.secret_sauce <- false
+  | "spatula" ->
+      p.spatula <-
+        (pr.spatula_price <- pr.spatula_price |> increase_price;
+         p.spatula + 1)
+  | "grilling dad" ->
+      p.grilling_dad <-
+        (pr.grilling_dad_price <- pr.grilling_dad_price |> increase_price;
+         p.grilling_dad + 1)
+  | "burger tree" ->
+      p.burger_tree <-
+        (pr.burger_tree_price <- pr.burger_tree_price |> increase_price;
+         p.burger_tree + 1)
+  | "food truck" ->
+      p.food_truck <-
+        (pr.food_truck_price <- pr.food_truck_price |> increase_price;
+         p.food_truck + 1)
+  | "burger lab" ->
+      p.burger_lab <-
+        (pr.burger_lab_price <- pr.burger_lab_price |> increase_price;
+         p.burger_lab + 1)
+  | "burger wormhole" ->
+      p.burger_wormhole <-
+        (pr.burger_wormhole_price <- pr.burger_wormhole_price |> increase_price;
+         p.burger_wormhole + 1)
+  | _ -> failwith "Unknown item attempt"
--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,13 @@
 module R = Raylib
 module H = Help
 
-let stats = H.init
+let burger_stats = H.burger_init
+let item_stats = H.item_init
+let price_list = H.item_price_init
 
 (*move state to stats later*)
 let state = ref 0
+let time = ref 0
 
 let setup () =
   R.init_window 1200 800 "Burger Clicker";
@@ -18,6 +21,14 @@ let setup () =
   (bg_texture, burger_texture)
 
 let burger_hitbox = R.Rectangle.create 425. 350. 100. 100.
+let sauce_hitbox = R.Rectangle.create 940. 130. 114. 149.
+let secret_sauce_hitbox = R.Rectangle.create 1060. 130. 134. 149.
+let spatula_hitbox = R.Rectangle.create 940. 284. 254. 75.
+let grilling_dad_hitbox = R.Rectangle.create 940. 365. 254. 74.
+let burger_tree_hitbox = R.Rectangle.create 940. 445. 254. 74.
+let food_truck_hitbox = R.Rectangle.create 940. 525. 254. 74.
+let burger_lab_hitbox = R.Rectangle.create 940. 605. 254. 74.
+let burger_wormhole_hitbox = R.Rectangle.create 940. 685. 254. 109.
 
 let rec loop texture =
   let bg = fst texture in
@@ -29,6 +40,11 @@ let rec loop texture =
       R.clear_background R.Color.raywhite;
       R.draw_texture bg 0 0 R.Color.raywhite;
       R.draw_texture burger 425 350 R.Color.raywhite;
+      time := !time + 1;
+      if !time = 60 then (
+        time := 0;
+        H.increment_burger_bps burger_stats)
+      else ();
       if R.is_mouse_button_down R.MouseButton.Left = false then state := 0;
       let mouse_point = R.get_mouse_position () in
       if R.check_collision_point_rec mouse_point burger_hitbox then
@@ -36,10 +52,97 @@ let rec loop texture =
         if R.is_mouse_button_down R.MouseButton.Left then
           if !state = 0 then (
             state := 1;
-            H.increment_burger stats);
+            H.increment_burger_click burger_stats);
+
+      (*The following hitbox detections should probably be moved into another
+        function called shop or something like that*)
+      if R.check_collision_point_rec mouse_point sauce_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if
+            !state = 0
+            && burger_stats.burgers > price_list.sauce_price
+            && item_stats.sauce
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.sauce_price;
+            H.increment_item item_stats "sauce" price_list;
+            H.increment_click_power burger_stats 2);
+
+      if R.check_collision_point_rec mouse_point secret_sauce_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if
+            !state = 0
+            && burger_stats.burgers > price_list.secret_sauce_price
+            && item_stats.secret_sauce
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.secret_sauce_price;
+            H.increment_item item_stats "secret sauce" price_list;
+            H.increment_click_power burger_stats 10);
+
+      if R.check_collision_point_rec mouse_point spatula_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 && burger_stats.burgers > price_list.spatula_price then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.spatula_price;
+            H.increment_item item_stats "spatula" price_list;
+            H.increment_bps burger_stats "spatula");
+
+      if R.check_collision_point_rec mouse_point grilling_dad_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 && burger_stats.burgers > price_list.grilling_dad_price
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.grilling_dad_price;
+            H.increment_item item_stats "grilling dad" price_list;
+            H.increment_bps burger_stats "grilling dad");
+
+      if R.check_collision_point_rec mouse_point burger_tree_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 && burger_stats.burgers > price_list.burger_tree_price
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.burger_tree_price;
+            H.increment_item item_stats "burger tree" price_list;
+            H.increment_bps burger_stats "burger tree");
+
+      if R.check_collision_point_rec mouse_point food_truck_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 && burger_stats.burgers > price_list.food_truck_price
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.food_truck_price;
+            H.increment_item item_stats "food truck" price_list;
+            H.increment_bps burger_stats "food truck");
+
+      if R.check_collision_point_rec mouse_point burger_lab_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if !state = 0 && burger_stats.burgers > price_list.burger_lab_price
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats price_list.burger_lab_price;
+            H.increment_item item_stats "burger lab" price_list;
+            H.increment_bps burger_stats "burger lab");
+
+      if R.check_collision_point_rec mouse_point burger_wormhole_hitbox then
+        if R.is_mouse_button_down R.MouseButton.Left then
+          if
+            !state = 0
+            && burger_stats.burgers > price_list.burger_wormhole_price
+          then (
+            state := 1;
+            H.decrease_burger_spend burger_stats
+              price_list.burger_wormhole_price;
+            H.increment_item item_stats "burger wormhole" price_list;
+            H.increment_bps burger_stats "burger wormhole");
+
       R.draw_text
-        (string_of_int stats.burgers)
+        (string_of_int burger_stats.burgers)
         425 30 100
+        (R.Color.create 50 42 79 255);
+      R.draw_text
+        (string_of_int burger_stats.bps)
+        415 120 40
         (R.Color.create 50 42 79 255);
       R.end_drawing ();
       loop texture
